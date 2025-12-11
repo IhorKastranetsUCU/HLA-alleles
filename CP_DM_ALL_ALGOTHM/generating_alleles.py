58724d58b6ffb26d1aeca7ac3d2ead869cd9ffac
er1973 <s.konoval.pn@ucu.edu.ua>
2025-12-11 21:56:48 +0200

    Create generating_alleles.py

---
+++ CP_DM_ALL_ALGOTHM/generating_alleles.py
@@ -0,0 +1,59 @@
+import random
+
+LOCUS_LIST = ["A", "B", "DR"]
+VALID_ALLELES = {
+    "A": [
+        "01", "02", "03", "11", "23", "24", "25", "26", "29", "30",
+        "31", "32", "33", "68"
+    ],
+
+    "B": [
+        "07", "08", "13", "14", "15", "18", "27", "35", "37", "38",
+        "39", "40", "41", "44", "49", "50", "51", "52", "53", "55",
+        "57", "58"
+    ],
+
+    "DR": [
+        "01", "03", "04", "07", "08", "11", "12", "13",
+        "14", "15", "16"
+    ]
+}
+
+def get_realistic_subtype():
+  '''
+  make realistic subtype for allele
+  '''
+    subtypes = [
+        "01",               # Домінантний
+        "02", "03", "04",   # Дуже часті
+        "05", "06", "07",   # Часті
+        "08", "09", "10",   # Рідші
+        "11", "12", "13"    # Ще рідші, але реальні
+    ]
+
+    # Ваги (Weights) - визначають, як часто випадає кожне число.
+    # Логіка: :01 випадає дуже часто, :13 - дуже рідко.
+    weights = [
+        60,                 # 01 - (найбільша вага, ~60% шансу)
+        15, 10, 5,          # 02-04
+        3, 2, 1,            # 05-07
+        1, 0.5, 0.5,        # 08-10
+        0.3, 0.3, 0.3       # 11-13 (дуже рідко)
+    ]
+    chosen_subtype = random.choices(subtypes, weights=weights)[0]
+    return chosen_subtype
+
+
+def generate_person_alleles():
+  '''
+  generates persons alleles
+  '''
+    person_alleles = set()
+    for locus in LOCUS_LIST:
+        for i in range(2):
+            if i != 1 or random.random() >= homozygosity_rate:
+                group = random.choice(VALID_ALLELES[locus])
+                spec_prot = get_realistic_subtype()
+                allele = f"{locus}*{group:02}:{spec_prot:02}"
+                person_alleles.add(allele)
+    return person_alleles
